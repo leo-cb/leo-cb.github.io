1bb9f95a26c151f91afd3550f04dd12392df4489
Leonardo Barata <leo.cb92@hotmail.com>
2023-09-07 00:10:19 +0100

    updated config

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -87,11 +87,19 @@ const config = {
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Heart Disease Prediction Web app',
+      title: 'Heart Disease Prediction Website',
       description:
         'Flask application that predicts heart disease (and explains the prediction) from a set of relevant patient information, \
-        derived from a GBT ML Model. Deployed from this <a href="https://github.com/leo-cb/HeartDiseasePrediction_WebApp">repo</a>.',
-      imageUrl: 'https://via.placeholder.com/250x250',
+        derived from a GBT ML Model. Deployed from "HeartDiseasePrediction_WebApp" repo available in my Git. Technology stack:\
+        PySpark\
+        MLFlow\
+        AutoML\
+        Shap for model explainability\
+        Flask for rest API and website\
+        Docker\
+        CI/CD with Github actions\
+        NoSQL (MongoDB)',
+      imageUrl: 'https://i.ibb.co/f2W9Yz7/homepage.png',
       link: 'http://149.56.100.90:5000',
     },
   ],
